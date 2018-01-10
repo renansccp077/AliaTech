777086b1bd054464f5b420358da364121a1eeb65
francisco <francisco@10.180.41.3>
2018-01-10 12:38:42 -0300

    Classes ProdutoDAO e ClienteDAO

--- alBebidas/src/br/com/DAO/ClienteDAO.java
+++ alBebidas/src/br/com/DAO/ClienteDAO.java
@@ -5,40 +5,37 @@
  */
 package br.com.DAO;
 
-import br.com.classes.Produto;
+import br.com.classes.Cliente;
 import br.com.connection.ConnectionFactory;
 import com.mysql.jdbc.PreparedStatement;
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 /**
  *
  * @author francisco
  */
-public class ProdutoDAO {
-    public void create(Produto p){
+public class ClienteDAO {
+
+    public void create(Cliente c) {
         Connection con = ConnectionFactory.getConnection();
         PreparedStatement stmt = null;
-        
+
         try {
-            stmt = (PreparedStatement) con.prepareStatement("INSERT INTO Produto (NomeProduto, PrecoVenda, PrecoCompra, QtdEstoque) VALUES (?,?,?,?)");
-            stmt.setString(1, p.getNomeProduto());
-            stmt.setFloat(2, p.getPrecoVenda());
-            stmt.setFloat(3, p.getPrecoCompra());
-            stmt.setInt(4, p.getQtdEstoque());
-            
+            stmt = (PreparedStatement) con.prepareStatement("INSERT INTO Produto (NomeCliente, cpf, telefone, endereco) VALUES (?,?,?,?)");
+            stmt.setString(1, c.getNomeCliente());
+            stmt.setString(2, c.getCpfCliente());
+            stmt.setString(3, c.getTelefoneCliente());
+            stmt.setString(4, c.getEnderecoCliente());
+
             stmt.executeUpdate();
-            
+
             JOptionPane.showMessageDialog(null, "Salvo com Sucesso!");
         } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "Erro ao salvar: "+ex);
-        }finally{
+            JOptionPane.showMessageDialog(null, "Erro ao salvar: " + ex);
+        } finally {
             ConnectionFactory.closeConnection(con, stmt);
         }
-        
-        
     }
 }
